416b8cf1b3e9f125a1b9160e217f742da7f6b88c
waidesoper <waide.soper@gmail.com>
2021-11-30 20:15:56 +1100

    Tidied up, gave a way to turn it into your own ender pearl, gave it a texture, made it glint when armed gave it tool-tips

--- src/main/java/waidesoper/someoneelses/SomeoneElses.java
+++ src/main/java/waidesoper/someoneelses/SomeoneElses.java
@@ -2,20 +2,17 @@ package waidesoper.someoneelses;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
-import net.minecraft.entity.EntityType;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import waidesoper.someoneelses.item.SomeoneElsesEnderPearl;
-import waidesoper.someoneelses.item.entity.SomeoneElsesEnderPearlEntity;
 
 public class SomeoneElses implements ModInitializer {
     public static String MOD_ID = "someone-elses";
     public static Logger LOGGER = LogManager.getLogger();
-    public static final SomeoneElsesEnderPearl SEEP = new SomeoneElsesEnderPearl(new FabricItemSettings().group(ItemGroup.MISC).maxCount(1));
+    public static final SomeoneElsesEnderPearl SEEP = new SomeoneElsesEnderPearl(new FabricItemSettings().group(ItemGroup.MISC).maxCount(16));
 
     @Override
     public void onInitialize() {
--- src/main/java/waidesoper/someoneelses/item/SomeoneElsesEnderPearl.java
+++ src/main/java/waidesoper/someoneelses/item/SomeoneElsesEnderPearl.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.thrown.EnderPearlEntity;
 import net.minecraft.item.EnderPearlItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -40,13 +41,15 @@ public class SomeoneElsesEnderPearl extends EnderPearlItem {
         if(!world.isClient) {
             if (user.isSneaking()) {
                 HitResult target = MinecraftClient.getInstance().crosshairTarget;
-                SomeoneElses.LOGGER.info(target.getType().name());
                 if(target.getType()==HitResult.Type.ENTITY){
                     EntityHitResult entityHit = (EntityHitResult) target;
                     Entity entity = entityHit.getEntity();
                     if(entity instanceof PlayerEntity){
                         setOwner(itemStack, (PlayerEntity) entity);
                     }
+                    return TypedActionResult.success(itemStack,world.isClient());
+                } else if (target.getType() == HitResult.Type.MISS){
+                    setOwner(itemStack, user);
                     return TypedActionResult.success(itemStack,world.isClient());
                 }
             } else {
@@ -58,7 +61,6 @@ public class SomeoneElsesEnderPearl extends EnderPearlItem {
                     world.playSound(null, owner.getX(), owner.getY(), owner.getZ(), SoundEvents.ENTITY_ENDER_PEARL_THROW, SoundCategory.NEUTRAL, 0.5f, 0.4f / (world.getRandom().nextFloat() * 0.4f + 0.8f));
                     user.getItemCooldownManager().set(this, 20);
                     if (!world.isClient) {
-                        SomeoneElses.LOGGER.info(owner.getDisplayName());
                         EnderPearlEntity enderPearlEntity = new EnderPearlEntity(world, (ServerPlayerEntity) owner);
                         enderPearlEntity.setItem(itemStack);
                         enderPearlEntity.setProperties(user, user.getPitch(), user.getYaw(), 0.0f, 1.5f, 1.0f);
@@ -75,6 +77,23 @@ public class SomeoneElsesEnderPearl extends EnderPearlItem {
         return TypedActionResult.fail(itemStack);
     }
 
+    @Override
+    public boolean hasGlint(ItemStack itemStack){
+        if(itemStack.hasNbt()){
+            return itemStack.getNbt().getUuid("owner") != null;
+        }
+        return false;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext){
+        if(itemStack.hasNbt()){
+            tooltip.add(new TranslatableText("item.someone-elses.seepwithowner",
+                    world.getPlayerByUuid(itemStack.getOrCreateNbt().getUuid("owner")).getDisplayName()));
+        } else {
+            tooltip.add(new TranslatableText("item.someone-elses.seepnoowner"));
+        }
+    }
     public void setOwner(ItemStack itemStack, PlayerEntity entity){
         itemStack.getOrCreateNbt().putUuid("owner", entity.getUuid());
     }
